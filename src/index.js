d0b6d3d82c0dc5cf301a8200ad27fbe00138481f
jiucool <jiucool@qq.com>
2024-06-26 19:47:07 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,14 +7,14 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
+  "docker.jiucool.net": dockerHub,
+  "quay.jiucool.net": "https://quay.io",
+  "gcr.jiucool.net": "https://gcr.io",
+  "k8s-gcr.jiucool.net": "https://k8s.gcr.io",
+  "k8s.jiucool.net": "https://registry.k8s.io",
+  "ghcr.jiucool.net": "https://ghcr.io",
+  "cloudsmith.jiucool.net": "https://docker.cloudsmith.io",
+  "ecr.jiucool.net": "https://public.ecr.aws",
 
   // staging
   "docker-staging.libcuda.so": dockerHub,
